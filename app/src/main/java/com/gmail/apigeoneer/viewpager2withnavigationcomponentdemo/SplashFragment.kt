09e85345e2d15505d62ec993eff3371f10de0a23
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-03-03 11:36:50 +0530

    Added the Splash Fragment to the nav graph. SplashFragment -> Navigated to the ViewPagerFragment using a Handler().

--- app/src/main/java/com/gmail/apigeoneer/viewpager2withnavigationcomponentdemo/SplashFragment.kt
+++ app/src/main/java/com/gmail/apigeoneer/viewpager2withnavigationcomponentdemo/SplashFragment.kt
@@ -1,10 +1,12 @@
 package com.gmail.apigeoneer.viewpager2withnavigationcomponentdemo
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 class SplashFragment : Fragment() {
 
@@ -12,6 +14,15 @@ class SplashFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        /*
+         * Navigate from the splash screen to the ViewPagerFragment after 2 secs using a Handler()
+         * Make sure you 've added the ViewPagerFragment to the nav graph & have connected the SplashFragment to it
+         */
+        Handler().postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_viewPagerFragment)
+        }, 2000)
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
