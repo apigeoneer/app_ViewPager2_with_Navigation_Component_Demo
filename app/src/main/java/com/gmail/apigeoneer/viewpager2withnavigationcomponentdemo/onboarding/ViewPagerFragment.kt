faabd4eba53f0ec50855e2b369e3b8293b5b21f8
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-03-03 11:06:58 +0530

    Just applying the kotlin-android-extensions plugin fixed the view.view_pager.adapter = adapter problem.

--- app/src/main/java/com/gmail/apigeoneer/viewpager2withnavigationcomponentdemo/onboarding/ViewPagerFragment.kt
+++ app/src/main/java/com/gmail/apigeoneer/viewpager2withnavigationcomponentdemo/onboarding/ViewPagerFragment.kt
@@ -5,23 +5,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import com.gmail.apigeoneer.viewpager2withnavigationcomponentdemo.databinding.FragmentViewPagerBinding
+import com.gmail.apigeoneer.viewpager2withnavigationcomponentdemo.R
 import com.gmail.apigeoneer.viewpager2withnavigationcomponentdemo.onboarding.screens.FirstFragment
 import com.gmail.apigeoneer.viewpager2withnavigationcomponentdemo.onboarding.screens.SecondFragment
 import com.gmail.apigeoneer.viewpager2withnavigationcomponentdemo.onboarding.screens.ThirdFragment
-
-
-//import kotlinx.android.synthetic.main.fragment_view_pager.view.*
+import kotlinx.android.synthetic.main.fragment_view_pager.view.*
 
 /**
  * Contains the data that you want to display
  */
 class ViewPagerFragment : Fragment() {
-
-    // data binding
-    private lateinit var binding: FragmentViewPagerBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,8 +24,6 @@ class ViewPagerFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_view_pager, container, false)
-
-       // viewPager = view.findViewById(R.id.view_pager)
 
         val fragmentList = arrayListOf<Fragment>(
                 FirstFragment(),
@@ -45,7 +37,7 @@ class ViewPagerFragment : Fragment() {
                 lifecycle
         )
 
-      //  view.viewPager.adapter = adapter // Synthetic view references have been deprecated and will not be supported in the future. *NEED TO USE DATA BINDING*
+        view.view_pager.adapter = adapter // Synthetic view references have been deprecated and will not be supported in the future. *NEED TO USE DATA BINDING*
 
         return view
     }
